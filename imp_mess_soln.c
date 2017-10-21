9fbddadf3d4cee9f4cf661442442eb90d2fd485d
Shubham Rastogi <shubhamrastogi3111995@gmail.com>
2017-10-21 18:02:02 -0400

    Written, need to fix bugs

--- imp_mess_soln.c
+++ imp_mess_soln.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
 int * * getAdjMatrix(int *);
 void printAdjMatrix(int * *, int);
 void destroyMatrix(int * *, int);
+int findMinTime(int * *, int);
+int findMinDistNode(int *, int *, int);
+int findMaxDist(int *, int);
 
 int * * getAdjMatrix(int * size)
 {
@@ -78,12 +82,69 @@ void destroyMatrix(int * * adj_mat, int size)
 	free(adj_mat);
 }
 
+int findMinTime(int * * adj_mat, int size)
+{
+	int * distSet = malloc(sizeof(int) * size);
+	int * visitSet = (int *) calloc(size, sizeof(int) * size);
+	int minNode, i, j, minTime;
+
+	distSet[0] = 0;
+	for(i = 1; i < size; i++)
+		distSet[i] = INT_MAX;
+	for(i = 0; i < (size - 1); i++)
+	{
+		minNode = findMinDistNode(distSet, visitSet, size);
+		visitSet[i] = 1;
+
+		for(j = 0; j < size; j++)
+		{
+			if(!visitSet[j] && adj_mat[minNode][j] > 0 && distSet[minNode] != INT_MAX && 
+				(distSet[minNode] + adj_mat[minNode][j]) < distSet[j])
+			{
+				distSet[j] = distSet[minNode] + adj_mat[minNode][j];
+			}
+		}
+	}
+
+	minTime = findMaxDist(distSet, size);
+	free(distSet);
+	free(visitSet);
+	return minTime;
+}
+
+int findMinDistNode(int * distSet, int * visitSet, int size)
+{
+	int i, minNode, min = INT_MAX;
+	for(i = 0; i < size; i++)
+	{
+		if(!visitSet[i] && distSet[i] < min)
+		{
+			minNode = i;
+			min = distSet[i];
+		}
+	}
+	return minNode;
+}
+
+int findMaxDist(int * distSet, int size)
+{
+	int i, max = INT_MIN;
+	for(i = 0; i < size; i++)
+	{
+		if(distSet[i] > max)
+			max = distSet[i];
+	}
+	return max;
+}
+
 int main(int argc, char * * argv)
 {
-	int size;
+	int size, minTime;
 	int * * temp = getAdjMatrix(&size);
 	printf("size is: %d\n", size);
 	printAdjMatrix(temp, size);
+	minTime = findMinTime(temp, size);
+	printf("The minimum time to get the message across is: %d\n", minTime);
 	destroyMatrix(temp, size);
 	return 0;
 }
